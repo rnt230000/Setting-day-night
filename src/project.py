b5ac75df056fadfff3b2c83290d244ded032b1fb
Rodney Tolbert <thecoolkidrt@gmail.com>
2026-04-27 03:27:26 -0500

    Updated button position & size: Increased the button_rect size and position to be easily seen.
    Button function:
    Created a check_click method in the button class. If the user left_click on a button_rect, it returns True. If not, it returns False.

--- src/project.py
+++ src/project.py
@@ -1,5 +1,17 @@
 import pygame
 
+
+class Day():
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.color = (92, 206, 250, 0.8)
+
+    def draw(self, surface):
+        rect = pygame.Rect((0,0), (self.width, self.height))
+        pygame.draw.rect(surface, self.color, rect)
+
 class Button():
 
     def __init__(self, text, x_pos, y_pos, enabled):
@@ -9,12 +21,21 @@ class Button():
         self.enabled = enabled
 
     def draw(self, surface):
-        font = pygame.font.Font('freesansbold.ttf', 18)
+        font = pygame.font.Font('freesansbold.ttf', 40)
         button_text = font.render(self.text, True, 'black')
-        button_rect = pygame.Rect((self.x_pos , self.y_pos), (150, 25))
+        button_rect = pygame.Rect((self.x_pos , self.y_pos), (150*2, 25*2))
         pygame.draw.rect(surface, 'gray', button_rect, 0, 5)
         pygame.draw.rect(surface, 'black', button_rect, 2, 5)
         surface.blit(button_text, (self.x_pos + 3, self.y_pos + 3))
+
+    def check_click(self):
+        mouse_pos = pygame.mouse.get_pos()
+        left_click = pygame.mouse.get_pressed()[0]
+        button_rect = pygame.Rect((self.x_pos , self.y_pos), (150*2, 25*2))
+        if left_click and button_rect.collidepoint(mouse_pos) and self.enabled:
+            return True
+        else:
+            return False
 
 
 
@@ -32,11 +53,21 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
         screen.fill("black")
-        day_button = Button('Day', screen.width // 4, screen.height / 1.5, True)
-        night_button = Button('Night', screen.width // 1.5, screen.height / 1.5, True)
+        # Buttons
+        day_button = Button('Day', screen.width // 5, screen.height / 1.3, True)
+        night_button = Button('Night', screen.width // 1.6, screen.height / 1.3, True)
+        print(day_button.check_click())
+        print(night_button.check_click())
+
+        day_bg = Day(screen.width, screen.height)
+
+        # Draw
+        day_bg.draw(screen)
         day_button.draw(screen)
         night_button.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(12)
     pygame.quit()
